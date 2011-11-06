7cc6ce3af2e5bebe229d137a861fec0efc6195e2
akitsukada <tsukada.akihiro@gmail.com>
2011-11-06 18:24:42 +0900

    fix typo

--- libtool.rb
+++ libtool.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Autoconf < Formula
+class Libtool < Formula
   url 'http://ftp.jaist.ac.jp/pub/GNU/libtool/libtool-2.4.tar.gz'
   homepage 'http://www.gnu.org/software/libtool/'
   md5 'b32b04148ecdd7344abc6fe8bd1bb021'
